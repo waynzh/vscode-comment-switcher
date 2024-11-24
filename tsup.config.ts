2a23bc0f0d332c279a333864e0fec5ab8a3f630a
waynzh <waynzh19@gmail.com>
2024-11-25 00:11:03 +0800

    chore: config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,8 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/extension.ts'],
   format: ['cjs'],
-  target: 'node18',
-  clean: true,
-  minify: true,
+  shims: false,
+  dts: false,
   external: [
     'vscode',
   ],
